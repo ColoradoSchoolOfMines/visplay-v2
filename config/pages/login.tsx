044c4857c15da167f4ab176ddae00ebbafe54e4b
AbruptSoftware <dylannichols702@gmail.com>
2022-09-28 16:55:28 -0600

    Beautified Login Page Styling
    - Adds uniform spacing between the elements on the page.
    - Enlarges the Visplay title text.

--- config/pages/login.tsx
+++ config/pages/login.tsx
@@ -2,9 +2,9 @@ import { Button, TextField } from "@mui/material";
 
 export default function Login() {
   return (
-    <div className="flex items-center flex-col ">
-      <div className="flex flex-col place-content-center h-screen w-64">
-        <h1 className="text-center">Visplay</h1>
+    <div className="flex items-center flex-col">
+      <div className="flex flex-col place-content-center h-screen w-64 space-y-10">
+        <h1 className="text-center text-5xl">Visplay</h1>
         <TextField label="Username" variant="outlined" className="bor" />
         <TextField label="Password" type="password" variant="outlined" />
         <Button href="home" variant="outlined">
