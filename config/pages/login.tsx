27648b398cb9668d70f2fb08dbcaa2da3fc806f8
AbruptSoftware <dylannichols702@gmail.com>
2022-09-28 21:46:59 -0600

    Visplay logo
    - Added the Visplay logo to the login screen.

--- config/pages/login.tsx
+++ config/pages/login.tsx
@@ -1,15 +1,21 @@
 import { Button, TextField } from "@mui/material";
+import next from "next";
+import VisplayLogo from "../../assets/icons/visplay_icon.svg"
+import Image from 'next/image'
 
 export default function Login() {
   return (
-    <div className="flex items-center flex-col">
-      <div className="flex flex-col place-content-center h-screen w-64 space-y-10">
-        <h1 className="text-center text-5xl">Visplay</h1>
-        <TextField label="Username" variant="outlined" className="bor" />
-        <TextField label="Password" type="password" variant="outlined" />
-        <Button href="home" variant="outlined">
-          Login
-        </Button>
+    <div>
+      <div className="flex items-center flex-col">
+        <div className="flex flex-col place-content-center h-screen w-64 space-y-10">
+          <Image src={VisplayLogo} width={400} height={200}/>
+          <h1 className="text-center text-5xl">Visplay</h1>
+          <TextField label="Username" variant="outlined" className="bor" />
+          <TextField label="Password" type="password" variant="outlined" />
+          <Button href="home" variant="outlined">
+            Login
+          </Button>
+        </div>
       </div>
     </div>
   );
