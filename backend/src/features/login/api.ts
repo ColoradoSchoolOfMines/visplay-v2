f30ae35f837486611da3c52d5a7db9036de6bea9
JoeArchibold <mojojoey5555@gmail.com>
2023-01-25 10:01:20 -0700

    Removed password from text

--- backend/src/features/login/api.ts
+++ backend/src/features/login/api.ts
@@ -4,7 +4,7 @@ import mongodb, { MongoClient } from 'mongodb';
 const loginApi = router();
 
 loginApi.get('/', async(req, res) => {
-    const url = 'mongodb+srv://jarchibold:BzWeSZG0b89JJpEX@cluster0.ovrf7th.mongodb.net/?retryWrites=true&w=majority';
+    const url = 'mongodb+srv://<username>:<password>@cluster0.ovrf7th.mongodb.net/?retryWrites=true&w=majority';
     const client = new MongoClient(url);
     await client.connect();
     const db = client.db("visplay");
