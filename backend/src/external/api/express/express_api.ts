96c0cc43bc72f443291944720f85eb2a90d14f72
JoeArchibold <mojojoey5555@gmail.com>
2023-02-07 18:30:12 -0700

    Attemping to access form data

--- backend/src/external/api/express/express_api.ts
+++ backend/src/external/api/express/express_api.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import { API } from "../api";
 import { loginApi } from './login_api';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 export class ExpressAPI implements API {
     startServer(hostname: string, port: number): void {
         const app = express();
         
         app.use(cors());
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
         app.use('/login', loginApi);
 
         app.listen(port, hostname);
--- backend/src/external/api/express/login_api.ts
+++ backend/src/external/api/express/login_api.ts
@@ -3,12 +3,9 @@ import router from 'express';
 const loginApi = router();
 
 loginApi.post('/', async(req, res) => {
-    // const url = 'mongodb+srv://<username>:<password>@cluster0.ovrf7th.mongodb.net/?retryWrites=true&w=majority';
-    // const client = new MongoClient(url);
-    // await client.connect();
-    // const db = client.db("visplay");
-    // res.send(await db.collection("users").find({}).toArray());
-    res.send("Hello!");
+    var body = {'name': req.body.username, 'test': 'user'};
+    console.log(req.body);
+    res.json(body);
 });
 
 export { loginApi };
